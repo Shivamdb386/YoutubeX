0758a4fbddc6001ad84a15603c1aab1d6927ee7a
Shivam <shivam.dubey2@ibm.com>
2025-06-22 20:29:50 +0530

    Added Footer

--- Frontend/src/MainPage.jsx
+++ Frontend/src/MainPage.jsx
@@ -31,6 +31,7 @@ const handleSignOut= ()=>{
 
 const userNavigation = [
   { name: 'Your Profile', href: '#' },
+
   { name: 'Sign out', href: '#', onClick: handleSignOut},
 ]
 
@@ -41,15 +42,6 @@ function classNames(...classes) {
 
 
   return (
-    <>
-      {/*
-        This example requires updating your template:
-
-        ```
-        <html class="h-full bg-gray-100">
-        <body class="h-full">
-        ```
-      */}
       <div className="min-h-full">
         <Disclosure as="nav" className="bg-gray-800">
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -194,9 +186,34 @@ function classNames(...classes) {
             <Outlet/>
           </div>
         </main>
-
+      <footer className="min-h-full dark:bg-gray-900">
+    <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+            <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">YoutubeX</span>
+            </a>
+            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                <li>
+                    <a href="#" className="hover:underline me-4 md:me-6">About</a>
+                </li>
+                <li>
+                    <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
+                </li>
+                <li>
+                    <a href="https://github.com/Shivamdb386" className="hover:underline me-4 md:me-6">Github</a>
+                </li>
+                <li>
+                    <a href="" className="hover:underline">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="https://flowbite.com/" className="hover:underline">YoutubeX™</a>. All Rights Reserved.</span>
+    </div>
+</footer>
         
       </div>
-    </>
+      
   )
 }
