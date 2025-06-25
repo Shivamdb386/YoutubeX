ccd75330747f08c6a850cf07d1e844c90f842484
Shivam <shivam.dubey2@ibm.com>
2025-06-26 02:45:37 +0530

    vercel and vite configs

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,23 +1,21 @@
-
-import { defineConfig, loadEnv} from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(({ command, mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
-    return{
-    server: {
-      proxy: {
-      '/api': {
-        target: env.VITE_API_BASE_URL,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-      },
-    },
-  plugins: [
-    tailwindcss(),
-  ],
-}
+    return {
+        server: command === 'serve' ? {
+            proxy: {
+                '/api': {
+                    target: env.VITE_API_BASE_URL,
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/api/, '')
+                }
+            },
+        } : undefined,
+        plugins: [tailwindcss()],
+        define: {
+            'process.env.VITE_API_BASE_URL': JSON.stringify(env.VITE_API_BASE_URL)
+        }
+    }
 })
-
-
